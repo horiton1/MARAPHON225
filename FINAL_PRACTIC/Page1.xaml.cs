474dfe9af57c2a2c577c0f32fd2e6c177575bb33
Pavel Smirnov <pavelsmirnov954@gmail.com>
2023-11-29 21:24:46 +0300

    Добавьте файлы проекта.

---
+++ FINAL_PRACTIC/Page1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FINAL_PRACTIC
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Nazad_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Uchastnik_Click(object sender, RoutedEventArgs e)
+        {
+            Raniy.Navigate(new Page2());
+        }
+
+        private void Noviy_Click(object sender, RoutedEventArgs e)
+        {
+            Raniy.Navigate(new Page3());
+        }
+    }
+}
